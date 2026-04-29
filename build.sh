5cfbdcbf5b0b93cba49170ddb70b6d1c5e3a1163
mihumilan <mihumilan@gmail.com>
2026-04-29 10:29:59 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,5 @@
 # Instalacja standardowych pakietów
 pip install -r requirements.txt
 
-# Bezpieczna instalacja przeglądarki bezpośrednio przez moduł Pythona
+# Instalacja przeglądarki Chromium
 python -m playwright install chromium
-
-# Instalacja brakujących paczek systemowych Linuxa (KLUCZOWE NA SERWERZE!)
-python -m playwright install-deps
